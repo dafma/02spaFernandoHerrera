acd3e9b5fc3dda389942ee74232cd47982487c36
dafma <mar_tp@hotmail.es>
2020-10-13 17:36:57 -0500

    add bootstrap and routes in angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+//  Rutas
+import { appRouting } from './app.routes';
+// Servicios
+
+
+// Componentes
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    appRouting
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+
+
+
+const appRoutes: Routes = [
+  { path : 'home', component: HomeComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
+
+export const appRouting = RouterModule.forRoot(appRoutes);
