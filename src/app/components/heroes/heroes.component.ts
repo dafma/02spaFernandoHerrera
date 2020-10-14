543318b732f917ca3693efa961b905cb9a249174
dafma <mar_tp@hotmail.es>
2020-10-14 09:55:42 -0500

    rutas con parametros

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from "../../servicios/heroes.service";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-heroes',
@@ -11,13 +13,17 @@ export class HeroesComponent implements OnInit {
   heroes: Heroe[] = [];
 
   // tslint:disable-next-line:variable-name
-  constructor( private _heroesService: HeroesService ) {
-    console.log('constructor');
+  constructor( private _heroesService: HeroesService,
+               private router: Router,
+              ) {
   }
 
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
-    console.log( this.heroes );
   }
 
+
+  verHeroe( idx: number ){
+    this.router.navigate( ['/heroe', idx] );
+  }
 }
